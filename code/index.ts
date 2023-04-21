7fdbdeaa93e807573392929b83ee95d1fbaf15ac
Diego Varzim <diego.varzim@hotmail.com>
2023-04-21 11:08:55 -0300

    ok

---
+++ code/index.ts
@@ -0,0 +1,9 @@
+
+const user = {
+    firstName: 'Diego',
+    age: 40,
+    lastName: 'S'
+}
+
+user.lastName = 'Varzim'
+
---
+++ prod/index.js
@@ -0,0 +1,7 @@
+"use strict";
+const user = {
+    firstName: 'Diego',
+    age: 40,
+    lastName: 'S'
+};
+user.lastName = 'Varzim';
