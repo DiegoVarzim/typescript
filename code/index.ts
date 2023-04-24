f65d40d3ee39553ca1b727d63d97027ad7cbb7e4
Diego Varzim <diego.varzim@hotmail.com>
2023-04-24 10:54:50 -0300

    ok

--- code/index.ts
+++ code/index.ts
@@ -1,9 +1,40 @@
 
-const user = {
-    firstName: 'Diego',
-    age: 40,
-    lastName: 'S'
+// const user = {
+//     firstName: 'Diego',
+//     age: 40,
+//     lastName: 'S'
+// }
+
+// user.lastName = 'Varzim'
+
+
+// // UNKNOWN
+
+// let itemInput: unknown
+// let itemName: string
+
+// itemInput = 10
+// itemInput = 'Apple'
+// if (itemInput === 'string') {
+//     itemName = itemInput
+// }
+
+
+// NEVER
+
+function generateError(message: string, code: number): never {
+    throw {message:message, errorCode: code}
 }
 
-user.lastName = 'Varzim'
+generateError('The application crashed', 500)
+console.log(generateError('The application crashed', 500))
+
+
+
+
+
+
+
+
+
 
--- prod/index.js
+++ prod/index.js
@@ -1,7 +1,21 @@
 "use strict";
-const user = {
-    firstName: 'Diego',
-    age: 40,
-    lastName: 'S'
-};
-user.lastName = 'Varzim';
+// const user = {
+//     firstName: 'Diego',
+//     age: 40,
+//     lastName: 'S'
+// }
+// user.lastName = 'Varzim'
+// // UNKNOWN
+// let itemInput: unknown
+// let itemName: string
+// itemInput = 10
+// itemInput = 'Apple'
+// if (itemInput === 'string') {
+//     itemName = itemInput
+// }
+// NEVER
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+}
+generateError('The application crashed', 500);
+console.log(generateError('The application crashed', 500));
